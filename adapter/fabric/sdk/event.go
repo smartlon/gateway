150695e45914c4183514e21606564072b00e30e3
smartlon <782802917@qq.com>
2019-12-27 09:30:48 +0800

    event

--- adapter/fabric/sdk/event.go
+++ adapter/fabric/sdk/event.go
@@ -11,33 +11,18 @@ import (
 	"github.com/hyperledger/fabric-sdk-go/pkg/client/event"
 	"github.com/hyperledger/fabric-sdk-go/pkg/common/providers/fab"
 	"github.com/pkg/errors"
-	"github.com/smartlon/gateway/adapter/iota/sdk"
+	//"github.com/smartlon/gateway/adapter/iota/sdk"
 	"sync"
 	"time"
 )
 
-
-type mamchannel struct {
-	Root    string     `json:",Root"`
-	SideKey string     `json:"SideKey"`
-}
-
-// logisticstrans type
-type logisticstrans struct {
-	//product might be food,fish,phone,other itmes
-	//Product id should be unique such as FISH123,Prawns456,ICECREAM789
-	ProductID         string       `json:"ProductID"`
-	ProductType       string       `json:"ProductType"`
-	SellerID          string       `json:"SellerID"`
-	SellerLocation    string       `json:"SellerLocation"`
-	BuyerID           string       `json:"BuyerID"`
-	BuyerLocation     string       `json:"BuyerLocation"`
-	LogisticsID       string       `json:"LogisticsID"`
-	LogisticsLocation string       `json:"LogisticsLocation"`
-	JourneyStartTime  string       `json:",JourneyStartTime"`
-	JourneyEndTime    string       `json:",JourneyEndTime"`
-	Status            string       `json:"Status"`
-	MAMChannel    mamchannel `json:"MAMChannel"`
+type IotaPayload struct {
+	ContainerID string `json:"ContainerID"`
+	Seed        string `json:"Seed"`
+	MamState    string `json:"MamState"`
+	Root        string `json:"Root"`
+	Mode       	string `json:"Mode"`
+	SideKey     string `json:"SideKey"`
 }
 
 func  listener(action *chaincodeInvokeAction,chaincode string,wg *sync.WaitGroup) error {
@@ -60,47 +45,47 @@ func  listener(action *chaincodeInvokeAction,chaincode string,wg *sync.WaitGroup
 		return err
 	}
 	defer  unregister(ec,[]fab.Registration{registrationCreateChannel,registrationDeliveryLogistics})
-	var logisticstran logisticstrans 
+	var iotapayload IotaPayload
 
 	select {
 	case ccEvent,ok := <-notifierCreateChannel:
 		if !ok {
 			return errors.WithMessage(err, "unexpected closed channel while waiting for chaincode event")
 		}
-		err := json.Unmarshal(ccEvent.Payload,&logisticstran)
+		err := json.Unmarshal(ccEvent.Payload,&iotapayload)
 		if err != nil {
 			fmt.Println(err.Error())
 		}
-		fmt.Printf("received chaincode event CreateChannel:  %v\n", logisticstran)
-		root, err:= sdk.CreateMAM(ccEvent.Payload,logisticstran.MAMChannel.SideKey)
-		if err !=nil {
-			fmt.Printf("fabric failed to create MAM:  %v\n", err.Error())
-		}
-		var argsArray []Args
-		argsArray = append(argsArray, Args{"InTransitLogistics",[]string{logisticstran.ProductID,root}})
-		_, err = action.invoke(Config().ChannelID, chaincode, argsArray)
-		if err !=nil {
-			fmt.Printf("fabric  failed to callback for InTransitLogistics :  %v\n", err.Error())
-		}
+		//fmt.Printf("received chaincode event CreateChannel:  %v\n", logisticstran)
+		//mamstat, root:= sdk.MAMTransmit(ccEvent.Payload,logisticstran.MAMChannel.SideKey)
+		//if err !=nil {
+		//	fmt.Printf("fabric failed to create MAM:  %v\n", err.Error())
+		//}
+		//var argsArray []Args
+		//argsArray = append(argsArray, Args{"InTransitLogistics",[]string{logisticstran.ProductID,root}})
+		//_, err = action.invoke(Config().ChannelID, chaincode, argsArray)
+		//if err !=nil {
+		//	fmt.Printf("fabric  failed to callback for InTransitLogistics :  %v\n", err.Error())
+		//}
 	case ccEvent,ok := <-notifierDeliveryLogistics:
 		if !ok {
 			return errors.WithMessage(err, "unexpected closed channel while waiting for chaincode event")
 		}
-		err := json.Unmarshal(ccEvent.Payload,&logisticstran)
+		err := json.Unmarshal(ccEvent.Payload,&iotapayload)
 		if err != nil {
 			fmt.Println(err.Error())
 		}
-		fmt.Printf("received chaincode event DeliveryLogistics:  %v\n", logisticstran)
-		_,temperature,err := sdk.BlockMAM(ccEvent.Payload,logisticstran.MAMChannel.Root,logisticstran.MAMChannel.SideKey)
-		if err !=nil {
-			fmt.Printf("fabric failed to block MAM:  %v\n", err.Error())
-		}
-		var argsArray []Args
-		argsArray = append(argsArray, Args{"SignLogistics",[]string{logisticstran.ProductID,temperature}})
-		_, err = action.invoke(Config().ChannelID, chaincode, argsArray)
-		if err !=nil {
-			fmt.Printf("fabric  failed to callback for SignLogistics :  %v\n", err.Error())
-		}
+		fmt.Printf("received chaincode event DeliveryLogistics:  %v\n", iotapayload)
+		//_,temperature,err := sdk.BlockMAM(ccEvent.Payload,logisticstran.MAMChannel.Root,logisticstran.MAMChannel.SideKey)
+		//if err !=nil {
+		//	fmt.Printf("fabric failed to block MAM:  %v\n", err.Error())
+		//}
+		//var argsArray []Args
+		//argsArray = append(argsArray, Args{"SignLogistics",[]string{logisticstran.ProductID,temperature}})
+		//_, err = action.invoke(Config().ChannelID, chaincode, argsArray)
+		//if err !=nil {
+		//	fmt.Printf("fabric  failed to callback for SignLogistics :  %v\n", err.Error())
+		//}
 	case <-time.After(time.Second * 3):
 		fmt.Println("Exit while waiting for chaincode event")
 		}
