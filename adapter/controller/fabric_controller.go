8c562540ca6a9877ea6be399f6c6cf5008e918fe
smartlon <782802917@qq.com>
2020-01-08 22:00:17 +0800

    fix bug

--- adapter/controller/fabric_controller.go
+++ adapter/controller/fabric_controller.go
@@ -77,37 +77,74 @@ func (lc *LogisticsController) QueryContainer(){
 	lc.Data["json"] = map[string]interface{}{"code": code,"msg": message, "data": ret}
 	lc.ServeJSON()
 }
-type QueryResponse struct {
+type ContainerQueryResponse struct {
 	Key    string `json:"Key"`
-	Record    string `json:"Record"`
+	Record    Container `json:"Record"`
+}
+type Container struct {
+	ContainerID string `json:"ContainerID"`
+	Description string `json:"Description"`
+	Timestamp string `json:"Timestamp"`
+	Location  string `json:"Location"`
+	Used      string   `json:"Used"`  //true is used and false is not used
+	Holder  string `json:"Holder"`
 }
 
 func (lc *LogisticsController) QueryAllContainers(){
 	queryAllContainersReqBytes := lc.Ctx.Input.RequestBody
 	code, message, ret := invokeController(queryAllContainersReqBytes)
-	var qr []QueryResponse
+	var qr []ContainerQueryResponse
 	err := json.Unmarshal([]byte(ret),&qr)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 	count := len(qr)
-	var resp []string
+	var resp []Container
 	for _,v := range qr {
 		resp = append(resp,v.Record)
 	}
 	lc.Data["json"] = map[string]interface{}{"code": code,"count": count,"msg": message, "data": resp}
 	lc.ServeJSON()
 }
+
+type logisticstransQueryResponse struct {
+	Key    string `json:"Key"`
+	Record    logisticstrans `json:"Record"`
+}
+
+type logisticstrans struct {
+	//product might be food,medical,other itmes
+	//Product id should be unique such as FISH123,Prawns456,ICECREAM789
+	LogisticstranID   string       `json:"LogisticstranID"`
+	ProductID         string       `json:"ProductID"`
+	ProductType       string       `json:"ProductType"`
+	SellerID          string       `json:"SellerID"`
+	SellerLocation    string       `json:"SellerLocation"`
+	BuyerID           string       `json:"BuyerID"`
+	BuyerLocation     string       `json:"BuyerLocation"`
+	LogisticsID       string       `json:"LogisticsID"`
+	LogisticsLocation string       `json:"LogisticsLocation"`
+	JourneyStartTime  string       `json:",JourneyStartTime"`
+	JourneyEndTime    string       `json:",JourneyEndTime"`
+	Status            string       `json:"Status"`
+	Count            string       `json:"Count"`
+	MAMChannel        MAMChannel   `json:"MAMChannel"`
+}
+type MAMChannel struct {
+	ContainerID       string       `json:"ContainerID"`
+	Root              string       `json:"Root"`
+	SideKey           string       `json:"SideKey"`
+}
 func (lc *LogisticsController) QueryAllLogistics(){
 	queryAllLogisticsReqBytes := lc.Ctx.Input.RequestBody
 	code, message, ret := invokeController(queryAllLogisticsReqBytes)
-	var qr []QueryResponse
+	var qr []logisticstransQueryResponse
 	err := json.Unmarshal([]byte(ret),&qr)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 	count := len(qr)
-	var resp []string
+	var resp []logisticstrans
 	for _,v := range qr {
 		resp = append(resp,v.Record)
 	}
